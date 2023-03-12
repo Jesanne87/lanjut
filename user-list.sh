5a00f4b9b8d92d84dce634f8115bec77063c304f
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-12 12:00:11 +0800

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -1,33 +1,15 @@
+# Created by JSphantom
 #!/bin/bash
 #wget https://github.com/${GitUser}/
 GitUser="Jesanne87"
 #IZIN SCRIPT
+# // LINE COLOUR
+export line=$(cat /etc/line)
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
 clear
-# Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-VALIDITY
-else
-echo -e "\e[31mPermission Denied!\e[0m";
-echo -e "\e[31mPlease buy script first\e[0m"
-exit 0
-fi
 echo -e "\e[32mloading...\e[0m"
-clear 
+clear
 if [ -f /etc/debian_version ]; then
 	UIDN=1000
 elif [ -f /etc/redhat-release ]; then
@@ -37,12 +19,9 @@ else
 fi
 clear
 echo " "
-echo " "
-echo "===========================================";
-echo " "
-echo "-----------------------------------"
+echo -e "\e[$line•───────────────────────────────•\e[m"
 echo "        USER ACCOUNTS LIST         "
-echo "-----------------------------------"
+echo -e "\e[$line•───────────────────────────────•\e[m"
 echo "[USERNAME]   -   [DATE EXPIRED]  "
 echo " "
 while read ceklist
@@ -55,9 +34,12 @@ do
         fi
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= '$UIDN' && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-echo "-------------------------------------"
+echo -e "\e[$line•───────────────────────────────•\e[m"
 echo "Number Of User Accounts: $JUMLAH USERS"
-echo "-------------------------------------"
+echo -e "\e[$line•───────────────────────────────•\e[m"
 echo " "
-echo "===========================================";
 echo " ";
+echo ""
+read -n1 -r -p "Press any key to continue..."
+    sleep 1
+    ssh
