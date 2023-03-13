17043664e04ff217d2694bafa3de46a81e8881d1
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-13 10:38:48 +0800

    Update info.sh

--- system/info.sh
+++ system/info.sh
@@ -5,13 +5,22 @@ GitUser="Jesanne87"
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
 clear
-yl='\e[031;1m'
-bl='\e[36;1m'
-gl='\e[32;1m'
-clear
-yl='\e[031;1m'
-bl='\e[36;1m'
-gl='\e[32;1m'
+# Color Validation
+Lred='\e[1;91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+green='\e[32m'
+RED='\033[0;31m'
+NC='\033[0m'
+BGBLUE='\e[1;44m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0;37m'
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
 clear
 echo -e "\e[1;32m══════════════════ \e[93mAutoscript PREMIUM\e[1;32m ══════════════════\e[0m" | tee -a log-install.txt
 echo ""
